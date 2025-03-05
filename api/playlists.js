f37ed2e11c4084b20b766b049ebc85d1e8a26de6
Nicole Riera <150852807+NicoRi5@users.noreply.github.com>
2025-03-04 20:57:08 -0500

    Create playlist.js protected routes

---
+++ api/playlists.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const router = express.Router();
+const { authenticate } = require("./auth.js");
+const prisma = (require = require("../prisma"));
+
+router.get("/playlists", authenticate, async (req, res, next) => {
+  const playlists = await prisma.playlist.findMany({
+    where: {
+      ownerId: req.user.id,
+    },
+  });
+  res.json(playlists);
+});
+router.post("/playlists", authenticate, async (req, res) => {
+  const { name, description, trackIds } = req.body;
+
+  const newPlaylist = await prisma.playlist.create({
+    data: {
+      name,
+      description,
+      ownerId: req.user.id,
+      tracks: {
+        connect: trackIds.map((id) => ({ id })),
+      },
+    },
+  });
+  res.json(newPlaylist);
+});
+
+router.get("/playlists/:id", authenticate, async (req, res) => {
+  const { id } = req.params;
+  
+    const playlist = await prisma.playlist.findUnique({
+      where: { id: parseInt(id) },
+      include: {
+        tracks: true,
+      },
+    });
+    if (playlist.ownerId !== req.user.id) {
+      return res.status(403).json({
+        error: "Forbidden! Logged in user does not own this playlist!",
+      });
+    }
+    res.json(playlist);
+});
+
+module.exports = router;
