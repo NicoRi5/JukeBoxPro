12c65247e90731715da6355b4fbb341f08036742
Nicole Riera <150852807+NicoRi5@users.noreply.github.com>
2025-03-04 21:29:16 -0500

    update server.js

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+const { router: authRouter } = require("./api/auth.js");
+const playlistRouter = require("./api/playlists.js");
+const trackRouter = require("./api/tracks.js");
+app.use(express.json());
+app.use("/auth", authRouter);
+app.use("/playlists", playlistRouter);
+app.use("/tracks", trackRouter);
+
+// the following helps with error handling and status code
+app.use((err, req, res, next) => {
+  console.error(err);
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "An error has occurred!";
+  res.status(statusCode).json({ error: message });
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
